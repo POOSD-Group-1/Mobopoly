02b4a368c11fc405a1064f95d60306253a09bb09
shane314 <47090961+shane314@users.noreply.github.com>
2024-03-14 02:34:00 -0400

    Testing function deploy through github

--- functions/index.js
+++ functions/index.js
@@ -27,7 +27,7 @@ initializeApp();
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
+exports.helloWorld2 = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
