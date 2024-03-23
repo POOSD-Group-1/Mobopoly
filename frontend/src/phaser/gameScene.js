dccc544d1eece110af5ab3ae7915619af82d73ed
Sachin S <ssk4988@gmail.com>
2024-03-23 17:35:44 -0400

    remove hardcoded baseURL

--- frontend/src/phaser/gameScene.js
+++ frontend/src/phaser/gameScene.js
@@ -5,8 +5,6 @@ class gameScene extends Phaser.Scene {
     super("gameScene");
   }
   preload() {
-    // this.load.setBaseURL("https://mobopoly-866b1--pr6-assets-be2efvj0.web.app/");
-    this.load.setBaseURL('http://localhost:3000');
     this.load.image("board", 'assets/board.png');
   }
   create() {
