0448f3b6c8b0bd0a232d366de5eab6ec4da88e5e
Sachin S <ssk4988@gmail.com>
2024-04-12 19:55:01 -0400

    add new board and fix anchors

--- frontend/src/data/board.js
+++ frontend/src/data/board.js
@@ -1,30 +1,96 @@
 import { Vector2 } from './util.js';
 import initialGameJSON from './initialGame.json';
 
+const minDim = Math.min(window.innerWidth, window.innerHeight);
+const boardWidth = minDim;
+const boardHeight = minDim;
+const referenceDim = 600;
+const scaleFactor = minDim / referenceDim;
 
-const boardWidth = 600;
-const boardHeight = 600;
+const playerDim = 20 * scaleFactor;
+
+
 
 const MAX_PLAYERS = 6;
 
 const center = new Vector2(boardWidth / 2, boardHeight / 2);
 
+let playerAnchorIndex = [];
+for (let i = 0; i < 8; i++) {
+    playerAnchorIndex.push(0);
+}
+for (let i = 0; i < 7 + 7 + 6; i++) {
+    playerAnchorIndex.push(1);
+}
 
 // the anchor for where the players should be placed on each location on the board
-const locationAnchors = [new Vector2(240, 215), new Vector2(180, 215), 
-    new Vector2(135, 215), new Vector2(90, 215), new Vector2(45, 215), new Vector2(0, 215), new Vector2(-45, 215)];
+const bottomRowY = 230;
+const bottomRowStartX = 183;
+const bottomRowOffset = -73.2;
+let locationAnchorsUnscaled = [new Vector2(260, bottomRowY), new Vector2(bottomRowStartX, bottomRowY)];
+for (let i = 0; i < 5; i++) {
+    locationAnchorsUnscaled.push(new Vector2(locationAnchorsUnscaled[locationAnchorsUnscaled.length - 1].x + bottomRowOffset, bottomRowY));
+}
+locationAnchorsUnscaled.push(new Vector2(-255, bottomRowY));
+const leftColumnX = -255;
+const leftColumnStartY = 185;
+const leftColumnOffset = -73.2;
+locationAnchorsUnscaled.push(new Vector2(leftColumnX, leftColumnStartY));
+for (let i = 0; i < 6; i++) {
+    locationAnchorsUnscaled.push(new Vector2(leftColumnX, locationAnchorsUnscaled[locationAnchorsUnscaled.length - 1].y + leftColumnOffset));
+}
+const topRowY = -253;
+const topRowOffset = -bottomRowOffset;
+const topRowStartX = -257 + topRowOffset;
+locationAnchorsUnscaled.push(new Vector2(topRowStartX, topRowY));
+for (let i = 0; i < 6; i++) {
+    locationAnchorsUnscaled.push(new Vector2(locationAnchorsUnscaled[locationAnchorsUnscaled.length - 1].x + topRowOffset, topRowY));
+}
+const rightColumnX = 255;
+const rightColumnOffset = -leftColumnOffset;
+const rightColumnStartY = topRowY + rightColumnOffset;
+locationAnchorsUnscaled.push(new Vector2(rightColumnX, rightColumnStartY));
+for (let i = 0; i < 6; i++) {
+    locationAnchorsUnscaled.push(new Vector2(rightColumnX, locationAnchorsUnscaled[locationAnchorsUnscaled.length - 1].y + rightColumnOffset));
+}
+const locationAnchors = locationAnchorsUnscaled.map((anchor) => anchor.clone().scale(scaleFactor));
 
 // where to put the players on each location relative to the location anchor
 // bottom row, left column, top row, right column
 // corners: ???
-const playerAnchors = [new Vector2(-10, 0), new Vector2(10, 0)];
 
+const playerAnchorsUnscaled = [
+    [new Vector2(-25, 0), new Vector2(0, 0), new Vector2(25, 0),
+        new Vector2(-25, 55), new Vector2(0, 55), new Vector2(25, 55)],
+    [new Vector2(-25, 0), new Vector2(0, 0), new Vector2(25, 0),
+        new Vector2(-25, 22), new Vector2(0, 22), new Vector2(25, 22)]
+];
+const playerAnchors = playerAnchorsUnscaled.map((anchorArr) => anchorArr.map((anchor)=>anchor.clone().scale(scaleFactor)));
+console.log(locationAnchorsUnscaled);
 function getLocationColor(location) {
-    if (initialGameJSON.properties[location].name.includes("Brown")){
+    if (initialGameJSON.properties[location].name.includes("Brown")) {
         return "#784D3C";
     }
-    else if(initialGameJSON.properties[location].name.includes("Light Blue")){
+    else if (initialGameJSON.properties[location].name.includes("Light Blue")) {
         return "#B9DAED";
+    }
+    else if (initialGameJSON.properties[location].name.includes("Magenta")) {
+        return "#A93A7F";
+    }
+    else if (initialGameJSON.properties[location].name.includes("Orange")) {
+        return "#D58A37";
+    }
+    else if (initialGameJSON.properties[location].name.includes("Red")) {
+        return "#BB292C";
+    }
+    else if (initialGameJSON.properties[location].name.includes("Yellow")) {
+        return "#FFF039";
+    }
+    else if (initialGameJSON.properties[location].name.includes("Green")) {
+        return "#5EA45C";
+    }
+    else if (initialGameJSON.properties[location].name.includes("Dark Blue")) {
+        return "#2C67A1";
     }
     else {
         return "transparent";
@@ -32,4 +98,4 @@ function getLocationColor(location) {
 }
 
 
-export { boardWidth, boardHeight, center, locationAnchors, playerAnchors, MAX_PLAYERS, getLocationColor };
+export { boardWidth, boardHeight, playerDim, scaleFactor, center, locationAnchors, playerAnchors, playerAnchorIndex, MAX_PLAYERS, getLocationColor };
--- frontend/src/data/util.js
+++ frontend/src/data/util.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import { scaleFactor } from './board';
 
 const Vector2 = Phaser.Math.Vector2;
 
@@ -6,4 +7,10 @@ function pieceImgFile(pieceNum) {
     return `assets/piece${pieceNum}.png`;
 }
 
-export { pieceImgFile, Vector2 };
+function resizeToDim(img, dim) {
+    const maxDim = Math.max(img.width, img.height);
+    const imgScaleFactor = dim / maxDim;
+    return img.setScale(imgScaleFactor);
+}
+
+export { pieceImgFile, resizeToDim, Vector2 };
